6dd4b421b692761dcdd0f948e1def7d4150af64d
Mariana Lopez <marianalopezeikilis@gmail.com>
2023-11-04 21:20:17 +0100

    Added removed files (mistake)

---
+++ firebase/clientApp.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCafR5mKntxJrlHl55ozUz6MpQt4HBGcmM",
+  authDomain: "eurofirms-project.firebaseapp.com",
+  projectId: "eurofirms-project",
+  storageBucket: "eurofirms-project.appspot.com",
+  messagingSenderId: "534009735125",
+  appId: "1:534009735125:web:efb1ea0a96ddbc7cdabf5c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

1917317d691571d4a8134cc08648e236eff82506
victoriaschwin <vicky.schwint95@gmail.com>
2023-11-08 20:53:09 +0100

    basic css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
-import styles from './page.module.css';
-import React from 'react';
+import styles from "./page.module.css";
+import React from "react";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-    <div>
-      <h1></h1>
-      <label htmlFor="">Usuario</label>
-      <input type="text" />
-      <label htmlFor="">Contraseña</label>
-      <input type="text" />
-      <button>Iniciar sesión</button>
-      <a href="">¿Olvidaste tu contraseña?</a>
-      <p>¿No tienes una cuenta?</p>
-      <a href="">Regístrate</a>
-
-    </div>
+      <div className={styles.container}>
+        <label htmlFor="">Usuario</label>
+        <input type="text" />
+        <label htmlFor="">Contraseña</label>
+        <input type="text" />
+        <button className={styles.btnblack}>Iniciar sesión</button>
+        <a href="">¿Olvidaste tu contraseña?</a>
+        <div>
+          <p>¿No tienes una cuenta?</p>
+          <a href="">Regístrate</a>
+        </div>
+      </div>
     </main>
-  )
+  );
 }

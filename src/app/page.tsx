a785c08963d041fa26c011d1ffb71340fdc31c8f
victoriaschwin <vicky.schwint95@gmail.com>
2023-11-09 13:30:53 +0100

    refactor to improve accesibility in login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,24 @@ import React from "react";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.container}>
-        <label htmlFor="">Usuario</label>
-        <input type="text" />
-        <label htmlFor="">Contraseña</label>
-        <input type="text" />
+      <section className={styles.login}>
+        <form className={styles.container}>
+          <label htmlFor="user" className={styles.label}>
+            Usuario
+          </label>
+          <input type="email" id="user" className={styles.input} />
+          <label htmlFor="password" className={styles.label}>
+            Contraseña
+          </label>
+          <input type="password" id="password" className={styles.input} />
+        </form>
         <button className={styles.btnblack}>Iniciar sesión</button>
-        <a href="">¿Olvidaste tu contraseña?</a>
-        <div>
+        <a href="" className={styles.a} >¿Olvidaste tu contraseña?</a>
+        <div className={styles.row}>
           <p>¿No tienes una cuenta?</p>
-          <a href="">Regístrate</a>
+          <a href="" className={styles.aRegister}>Regístrate</a>
         </div>
-      </div>
+      </section>
     </main>
   );
 }
